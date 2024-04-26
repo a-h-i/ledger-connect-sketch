ce17e9f6d4c8d66d5fa0202ae4fa97ef725746a5
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2024-04-26 17:06:06 +0100

    shows version and app name

--- src/transport.js
+++ src/transport.js
@@ -7,7 +7,19 @@ import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
 export async function getCurrentlyRunningModule() {
     const transport = await TransportWebUSB.create();
     const response = await transport.send(0xb0, 0x01, 0x00, 0x00);
-    console.log(response.length);
-    console.log('0x' + response.toString('hex'))
+    if (response[0] !== 1) {
+        console.error('Unsupported response format')
+    }
+
+    const runningComponentNameLength = response[1];
+    const componentNameEndIndex = runningComponentNameLength + 2;
+
+    const componentName = response.slice(2, componentNameEndIndex).toString('ascii');
+
+    const versionLength = response[componentNameEndIndex];
+    const versionEndIndex = componentNameEndIndex + 1 + versionLength;
+    const version = response.slice(componentNameEndIndex + 1, versionEndIndex).toString('ascii');
+
+    document.getElementById('runningModuleInfo').innerHTML = `Application: ${componentName} - Version: ${version}`
 }
 
